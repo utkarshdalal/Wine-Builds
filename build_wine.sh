c054f72ac84dd2bbc9fa268d1bbb18f1d615f0c0
airidosas252 <airidaslideikis@gmail.com>
2024-04-06 19:48:37 +0300

    build_wine.sh: use updated Pathfix patch for Staging builds too
    Hard to track each Wine branch and what changes they introduce. This should fix broken Wine-Staging builds.

--- build_wine.sh
+++ build_wine.sh
@@ -334,7 +334,11 @@ if [ "$TERMUX_GLIBC" = "true" ]; then
     if [ "$WINE_BRANCH" = "staging" ] || [ "$WINE_BRANCH" = "staging-tkg" ]; then
     patch -d wine -Np1 < "${scriptdir}"/esync.patch && \
     patch -d wine -Np1 < "${scriptdir}"/termux-wine-fix-staging.patch && \
+    if git -C "${BUILD_DIR}/wine" log | grep -q 4e04b2d5282e4ef769176c94b4b38b5fba006a06; then
+    patch -d wine -Np1 < "${scriptdir}"/pathfix-wine9.5.patch
+    else
     patch -d wine -Np1 < "${scriptdir}"/pathfix.patch
+    fi
     else
     patch -d wine -Np1 < "${scriptdir}"/esync.patch && \
     patch -d wine -Np1 < "${scriptdir}"/termux-wine-fix.patch && \
