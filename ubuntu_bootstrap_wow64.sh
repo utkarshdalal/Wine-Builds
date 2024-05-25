634f357951d1d8b54fe2932fd7ae8f0b152b9f0e
airidosas252 <airidaslideikis@gmail.com>
2024-05-25 15:10:57 +0300

    Update ubuntu_bootstrap_wow64.sh

--- ubuntu_bootstrap_wow64.sh
+++ ubuntu_bootstrap_wow64.sh
@@ -115,8 +115,8 @@ wget -O libpcap.tar.gz https://www.tcpdump.org/release/libpcap-${libpcap_version
 wget -O libxkbcommon.tar.xz https://xkbcommon.org/download/libxkbcommon-${libxkbcommon_version}.tar.xz
 if [ -d /usr/lib/x86_64-linux-gnu ]; then wget -O wine.deb https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/main/binary-amd64/wine-stable_9.0.0.0~jammy-1_amd64.deb; fi
 # VkD3D download script. Needs automation to be implemented
-wget -O https://dl.winehq.org/vkd3d/source/vkd3d-1.11.tar.xz 
-tar xf vkd3d-1.11.tar.xz
+wget -O vkd3d.tar.xz https://dl.winehq.org/vkd3d/source/vkd3d-1.11.tar.xz 
+tar xf vkd3d.tar.xz
 mv vkd3d-1.11 vkd3d
 #
 tar xf sdl.tar.gz
