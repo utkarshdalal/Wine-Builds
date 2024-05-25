9c09632be9675b85bc9d7b9469bbbda876658bc4
airidosas252 <airidaslideikis@gmail.com>
2024-05-25 10:39:14 +0300

    Update ubuntu_bootstrap_wow64.sh

--- ubuntu_bootstrap_wow64.sh
+++ ubuntu_bootstrap_wow64.sh
@@ -93,7 +93,7 @@ add-apt-repository -y ppa:ubuntu-toolchain-r/test
 add-apt-repository -y ppa:cybermax-dexter/mingw-w64-backport
 apt-get update
 apt-get -y build-dep wine-development libsdl2 libvulkan1
-apt-get -y install ccache gcc-14 g++-14 wget git gcc-mingw-w64 g++-mingw-w64
+apt-get -y install cmake flex bison ccache gcc-14 g++-14 wget git gcc-mingw-w64 g++-mingw-w64
 apt-get -y install libxpresent-dev libjxr-dev libusb-1.0-0-dev libgcrypt20-dev libpulse-dev libudev-dev libsane-dev libv4l-dev libkrb5-dev libgphoto2-dev liblcms2-dev libcapi20-dev
 apt-get -y install libjpeg62-dev samba-dev
 apt-get -y install libpcsclite-dev libcups2-dev
