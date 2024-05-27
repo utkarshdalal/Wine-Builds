928e3ccadce5b75e98baf3ae205edf842553a831
airidosas252 <airidaslideikis@gmail.com>
2024-05-27 12:36:05 +0300

    Update build_wine.sh
    Revert back some changes

--- build_wine.sh
+++ build_wine.sh
@@ -466,7 +466,11 @@ if [ "$TERMUX_GLIBC" = "true" ]; then
     echo "Applying address space patch"
     patch -d wine -Np1 < "${scriptdir}"/termux-wine-fix.patch && \
     echo "Applying path change patch"
-    patch -d wine -Np1 < "${scriptdir}"/pathfix.patch || {
+    if git -C "${BUILD_DIR}/wine" log | grep -q 4e04b2d5282e4ef769176c94b4b38b5fba006a06; then
+    patch -d wine -Np1 < "${scriptdir}"/pathfix-wine9.5.patch
+    else
+    patch -d wine -Np1 < "${scriptdir}"/pathfix.patch
+    fi || {
         echo "Error: Failed to apply one or more patches."
         exit 1
     }
