414a0f66c895a4012dce85cb9bc96017bf686eaa
airidosas252 <airidaslideikis@gmail.com>
2024-11-24 15:27:58 +0200

    Incorporate Bylaws virtual memory patch

--- build_wine.sh
+++ build_wine.sh
@@ -567,8 +567,7 @@ fi
 ### Experimental addition to address space hackery
 if [ "$TERMUX_GLIBC" = "true" ]; then
 echo "Applying additional address space patch... (credits to Bylaws)"
-wget -O address-space.patch https://github.com/bylaws/wine/commit/c12890cafb580764c076e4231636cafaf6e35089.patch
-patch -p1 < address-space.patch || {
+patch -p1 < "${scriptdir}"/wine-virtual-memory.patch || {
         echo "This patch did not apply. Stopping..."
 	exit 1
     }
