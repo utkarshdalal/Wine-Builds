78f1275d4687768c1395a43ee1066ec02d1eba94
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2025-10-05 12:05:54 +0530

    added libs to bootstrap

--- ubuntu_bootstrap_wow64.sh
+++ ubuntu_bootstrap_wow64.sh
@@ -98,7 +98,8 @@ apt-get -y install libxpresent-dev libjxr-dev libusb-1.0-0-dev libgcrypt20-dev l
 apt-get -y install libjpeg62-dev samba-dev libfreetype-dev libunwind-dev ocl-icd-opencl-dev libgnutls28-dev libx11-dev libxcomposite-dev libxcursor-dev libxfixes-dev libxi-dev libxrandr-dev 
 apt-get -y install libxrender-dev libxext-dev libpcsclite-dev libcups2-dev libosmesa6-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 apt-get -y install python3-pip libxcb-xkb-dev libfontconfig-dev libgl-dev libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libswresample-dev libavfilter-dev
-apt-get -y install meson ninja-build libxml2 libxml2-dev libxkbcommon-dev libxkbcommon0 xkb-data libxxf86vm-dev libdbus-1-dev
+apt-get -y install meson ninja-build libxml2 libxml2-dev libxkbcommon-dev libxkbcommon0 xkb-data libxxf86vm-dev libdbus-1-dev libharfbuzz-dev libpng-dev zlib1g-dev libbz2-dev libpulse-mainloop-glib0 libvpx-dev libopus-dev libtheora-dev yasm
+
 apt-get -y purge libvulkan-dev libvulkan1 mesa-vulkan-drivers vulkan-validationlayers vulkan-tools glslang-tools spirv-tools libsdl2-dev libsdl2-2.0-0 libpcap0.8-dev libpcap0.8 --purge --autoremove
 apt-get -y clean
 apt-get -y autoclean
